eeab82987fa37b52a57f4c6d6f9a2c2921856116
anirban007 <anirban.mandal007@gmail.com>
2021-12-12 02:24:16 +0530

    user management service added

---
+++ src/app/modules/user-management/user-management.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserManagementService {
+
+  constructor() { }
+
+  // TODO: Add mothods to call API
+}
