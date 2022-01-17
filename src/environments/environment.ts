409ebcb4f96a7c3862155c938693731974744b82
Sujoy2015 <sujoydebnath89@gmail.com>
2022-01-17 13:26:59 +0530

    update base url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
     production: false,
-    //baseUrl: 'https://e-storage.crownims.com/SODDMS/api/'
-    baseUrl: 'https://e-storage.crownims.com/SODDMSTesting/api/'
+    baseUrl: 'https://e-storage.crownims.com/SODDMS/api/'
+    //baseUrl: 'https://e-storage.crownims.com/SODDMSTesting/api/'
 };
 
 /*
